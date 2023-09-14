badc1d09219bdd5b8393a457974a1398a675a46e
MateusCor <142261895+MateusCor@users.noreply.github.com>
2023-09-13 23:32:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const meuBotao = document.getElementById("meuBotao")
+function redirecionarParaGoogle() {
+    window.location.href = "https://onedrive.live.com/?authkey=%21AEleeo0E1RV%5FhYE&id=8F74F936C76628EE%211277&cid=8F74F936C76628EE";
+}
+
+meuBotao.addEventListener("click", redirecionarParaGoogle)
