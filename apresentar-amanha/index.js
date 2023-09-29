1a1589e16d8f3d48832e4fcbaf1bf8aaff24942b
MateusCor <142261895+MateusCor@users.noreply.github.com>
2023-09-28 23:42:39 -0300

    Add files via upload

---
+++ apresentar-amanha/index.js
@@ -0,0 +1,7 @@
+const overlay = document.getElementById("overlay")
+function redirecionarParaGoogle() {
+    window.location.href = "https://onedrive.live.com/?authkey=%21AEleeo0E1RV%5FhYE&id=8F74F936C76628EE%211277&cid=8F74F936C76628EE";
+}
+
+overlay.addEventListener("click", redirecionarParaGoogle)
+
